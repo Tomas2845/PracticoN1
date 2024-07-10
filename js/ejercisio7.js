573717c704bc4aae6d7b3da90dcc8ee17d5bfa6e
Tomacho <tomacho28.th@gmail.com>
2024-07-10 14:20:43 -0300

    terminamos ejercisio 7

---
+++ js/ejercisio7.js
@@ -0,0 +1,12 @@
+const numero1 = parseInt (prompt("Ingrese un numero"));
+const numero2 = parseInt (prompt("Ingrese otro numero"));
+const numero3 = parseInt (prompt("Ingrese otro numero"));
+
+if (numero1 > numero2 ) {
+    document.write("El numero mayor es : " + numero1 );
+} else if  (numero2 > numero3) {
+    document.write ("el numero mayor es " + numero2)
+} else 
+{
+    document.write ("el numero mayor es " + numero3)
+}
