73dc9f53302535265412bc9ebb82348a40bd4839
Tomacho <tomacho28.th@gmail.com>
2024-07-11 00:12:16 -0300

    termine ejercisio 9

---
+++ js/ejercisio10.js
@@ -0,0 +1,12 @@
+let numero =  parseInt(prompt("Ingrese un numero"))
+
+if (numero % 2 === 0 ) 
+    {
+        document.write("el numero ingresado es divisible por dos ");
+    }
+else if (numero % 3 === 0) {
+    document.write("el numero ingresado no es divisible por 3");
+}
+
+el 
+
---
+++ js/ejercisio9.js
@@ -0,0 +1,16 @@
+let frase = prompt("Ingrese una frase").toLowerCase();
+let vocales = "";
+let i =0;
+
+while (i<=frase.length)
+    {
+        if (frase.charAt(i) === "a" || frase.charAt(i) === "e"  || frase.charAt(i) === "i" || frase.charAt(i) === "o" || frase.charAt(i) === "u" ) 
+            {   
+                vocales = vocales+frase.charAt(i)
+                
+            }
+            i++;
+}
+document.write("ingreso la frase: " + frase + " <br> y tiene estas vocales:  " + vocales )
+
+
