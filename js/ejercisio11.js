011adaea3e7c623e2044404a24340826dcd07563
Tomacho <tomacho28.th@gmail.com>
2024-07-11 10:45:01 -0300

    termianmos punto 10

---
+++ js/ejercisio11.js
@@ -0,0 +1,20 @@
+let numero = parseInt(prompt("Ingrese un numero"));
+
+
+if (numero % 2 === 0 ) 
+    {
+        document.write("el numero ingresado es divisible por 2  ");
+    }
+else if (numero % 3 === 0) {
+    document.write("el numero ingresado si es divisible por 3");
+}
+
+else if (numero % 5 === 0) {
+    document.write("el numero ingresado si es divisible por 5");
+}
+else if (numero % 7 === 0) {
+    document.write("el numero ingresado si es divisible por 7");
+}
+else {
+    document.write ("el" + numero + "si es divisible  por 2,3,5,7")
+}
